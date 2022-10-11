1287292b623adaabf313828832d0fc4696f249ee
fabeto10 <f0414475@gmail.com>
2022-10-11 13:52:32 -0400

    se agrega phpmyadmin y se corre interfaz en puerto 8080

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const routerApi = require('./routes');
-const { logErrors, errorHandler, boomErrorHandler } = require('./middleware/error.handler')
+const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middleware/error.handler')
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -30,6 +30,7 @@ app.get('/nueva-ruta', (req, res) => {
 routerApi(app);
 
 app.use(logErrors);
+app.use(ormErrorHandler);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
--- middleware/error.handler.js
+++ middleware/error.handler.js
@@ -1,3 +1,5 @@
+const { ValidationError } = require("sequelize");
+
 function logErrors (err, req, res, next) {
   console.log('logErrors')
   console.error(err);
@@ -12,6 +14,17 @@ function errorHandler (err, req, res, next) {
   });
 };
 
+function ormErrorHandler(err, req, res, next){
+  if(err instanceof ValidationError){
+    res.status(409).json({
+      statusCode: 409,
+      message: err.name,
+      errors: err.errors
+    });
+  }
+  next(err)
+}
+
 function boomErrorHandler (err, req, res, next) {
   if(err.isBoom){
     const {output} = err
@@ -20,4 +33,4 @@ function boomErrorHandler (err, req, res, next) {
   next(err);
 };
 
-module.exports = { logErrors, errorHandler, boomErrorHandler };
+module.exports = { logErrors, errorHandler, boomErrorHandler, ormErrorHandler };
